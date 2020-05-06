98fa62d6e7eaa1026836ee36e8856044c9a296ed
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-05-06 14:11:11 +0200

    Add PageBorder

---
+++ src/components/PageBorder/PageBorder.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './PageBorder.scss';
+
+export const PageBorder = () => {
+    return <div className="PageBorder">
+        <svg className="PageBorder__svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1872 865"
+             preserveAspectRatio="none">
+            <path className="PageBorder__border" fill="none" fillRule="evenodd" stroke="#ffffff" strokeWidth=".5"
+                  strokeDasharray="99999" strokeDashoffset="99999" d="M1872 0H0v865h1872z"
+            />
+        </svg>
+    </div>
+};
