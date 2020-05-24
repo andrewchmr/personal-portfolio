f8f18bcfaa2fad77b050cc3aae130816f14e5939
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-05-24 18:43:27 +0200

    Fix tooltip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import {Layout} from "../components/Layout";
 function Index() {
     const renderTooltips = () => {
         return (<>
-            <ReactTooltip className={'Index__tooltip'} uuid="my-photo" place="left" id='my-photo'>
+            <ReactTooltip className={'Index__tooltip'} arrowColor={'#121212'}
+                          uuid="my-photo" place="left" id='my-photo'>
                 <img className={'Index__photo Index__photo--vertical'}
                      alt={'Andriy Chemerynskiy Photo'}
                      src={'/static/personal-photo.jpg'}/>
             </ReactTooltip>
-            <ReactTooltip className={'Index__tooltip'} uuid="wroclaw-photo" place="right" id={'wroclaw-photo'}>
+            <ReactTooltip className={'Index__tooltip'} arrowColor={'#121212'}
+                          uuid="wroclaw-photo" place="right" id={'wroclaw-photo'}>
                 <img className={'Index__photo Index__photo--horizontal'}
                      alt={'Wroclaw, Poland Photo'}
                      src={'/static/wroclaw.jpg'}/>
@@ -21,9 +23,9 @@ function Index() {
     return (
         <Layout pageTitlePrefix={''}
                 description={'Hey, I’m Andriy Chemerynskiy, a Software Engineer currently based in Wroclaw, Poland.'}>
-            <div className={'Index'}>Hey, I’m <span data-tip={true} data-for="my-photo">Andriy Chemerynskiy</span>
-                , a Software Engineer currently based in <span data-tip={true}
-                                                               data-for="wroclaw-photo">Wroclaw, Poland</span>.
+            <div className={'Index'}>Hey, I’m <a data-tip={true} data-for="my-photo">Andriy Chemerynskiy</a>
+                , a Software Engineer currently based in <a data-tip={true}
+                                                            data-for="wroclaw-photo">Wroclaw, Poland</a>.
                 {renderTooltips()}
             </div>
         </Layout>
