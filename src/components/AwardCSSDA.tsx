0472c1e4a9d39a7482ab06ab991e8932602af5d7
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-06-16 21:37:14 +0200

    Update AwardCSSDA image

--- src/components/AwardCSSDA.tsx
+++ src/components/AwardCSSDA.tsx
@@ -4,8 +4,8 @@ export const AwardCSSDA = () => {
     return (
         <a href={'https://www.cssdesignawards.com/sites/andriy-chemerynskiy-portfolio/37283/'} target='_blank'>
             <img className={'AwardCSSDA'}
-                 src="https://www.cssdesignawards.com/images/2017/monogram/cssda-nominee-black-vote.png" width="110"
-                 alt="CSSDA WOTD Award Nominee Vote Monogram"/>
+                 src="https://www.cssdesignawards.com/images/2017/monogram/cssda-nominee-black.png" width="110"
+                 alt="CSSDA WOTD Award Nominee"/>
         </a>
     )
 };
