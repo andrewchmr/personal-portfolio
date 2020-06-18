fc16f21dfc2c6f6fafb9e836bff784ad9a95050c
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-06-18 12:06:00 +0200

    Minor fix

--- src/components/AwardCSSDA.tsx
+++ src/components/AwardCSSDA.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export const AwardCSSDA = () => {
+    const awardLink = 'https://www.cssdesignawards.com/sites/andriy-chemerynskiy-portfolio/37283/';
+
     return (
-        <a href={'https://www.cssdesignawards.com/sites/andriy-chemerynskiy-portfolio/37283/'} target='_blank'>
+        <a href={awardLink}
+           rel="noopener"
+           target='_blank'>
             <img className={'AwardCSSDA'}
-                 src="https://www.cssdesignawards.com/images/2017/monogram/cssda-nominee-black.png" width="110"
+                 src="https://www.cssdesignawards.com/images/2017/monogram/cssda-nominee-black.png"
+                 width="110"
                  alt="CSSDA WOTD Award Nominee"/>
         </a>
     )
