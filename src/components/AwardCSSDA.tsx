df214765ef164ceeb1c7ccbea1155518210c8f49
Andriy Chemerinskiy <andrzej.chem@gmail.com>
2020-06-21 14:50:29 +0200

    Update AwardCSSDA

--- src/components/AwardCSSDA.tsx
+++ src/components/AwardCSSDA.tsx
@@ -8,7 +8,7 @@ export const AwardCSSDA = () => {
            rel="noopener"
            target='_blank'>
             <img className={'AwardCSSDA'}
-                 src="https://www.cssdesignawards.com/images/2017/monogram/cssda-nominee-black.png"
+                 src="/static/cssda-special-kudos-black.png"
                  width="110"
                  alt="CSSDA WOTD Award Nominee"/>
         </a>
