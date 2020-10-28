6493e6261aa6a991dac8e78a606f68824d789c7a
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2020-10-28 11:07:20 +0100

    Update packages

--- src/components/AwardCSSDA.tsx
+++ src/components/AwardCSSDA.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import Image from 'next/image';
 
 export const AwardCSSDA = () => {
-    const awardLink = 'https://www.cssdesignawards.com/sites/andriy-chemerynskiy-portfolio/37283/';
+  const awardLink = 'https://www.cssdesignawards.com/sites/andriy-chemerynskiy-portfolio/37283/';
 
-    return (
-        <a href={awardLink}
-           rel="noopener"
-           target='_blank'>
-            <img className={'AwardCSSDA'}
-                 src="/static/cssda-special-kudos-black.png"
-                 width="110"
-                 alt="CSSDA WOTD Award Nominee"/>
-        </a>
-    )
+  return (
+    <a href={awardLink}
+      rel="noopener"
+      target='_blank'>
+      <span className={'AwardCSSDA'}>
+        <Image
+          src="/static/cssda-special-kudos-black.png"
+          width="110"
+          height="110"
+          alt="CSSDA WOTD Award Nominee" />
+      </span>
+    </a>
+  )
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,44 +2,45 @@ import React from "react";
 import ReactTooltip from "react-tooltip";
 import { Layout } from "../components/Layout";
 import { isMobile } from "../utils/isMobile";
+import Image from 'next/image';
 
 function Index() {
-    const renderTooltips = () => {
+  const renderTooltips = () => {
 
-        if (typeof navigator !== 'undefined' && isMobile()) return null;
+    if (typeof navigator !== 'undefined' && isMobile()) return null;
 
-        return (<>
-            <ReactTooltip className={'Index__tooltip'} arrowColor={'#121212'}
-                          uuid="my-photo" place="left" id='my-photo'>
-                <img className={'Index__photo Index__photo--vertical'}
-                     alt={'Andriy Chemerynskiy Photo'}
-                     src={'/static/personal-photo.jpg'}/>
-            </ReactTooltip>
-            <ReactTooltip className={'Index__tooltip'} arrowColor={'#121212'}
-                          uuid="wroclaw-photo" place="right" id={'wroclaw-photo'}>
-                <img className={'Index__photo Index__photo--horizontal'}
-                     alt={'Wroclaw, Poland Photo'}
-                     src={'/static/wroclaw.jpg'}/>
-            </ReactTooltip>
-        </>)
-    };
+    return (<>
+      <ReactTooltip className={'Index__tooltip'} arrowColor={'#121212'}
+        uuid="my-photo" place="left" id='my-photo'>
+        <Image width={400} height={500} className={'Index__photo Index__photo--vertical'}
+          alt={'Andriy Chemerynskiy Photo'}
+          src={'/static/personal-photo.jpg'} />
+      </ReactTooltip>
+      <ReactTooltip className={'Index__tooltip'} arrowColor={'#121212'}
+        uuid="wroclaw-photo" place="right" id={'wroclaw-photo'}>
+        <Image width={500} height={352} className={'Index__photo Index__photo--horizontal'}
+          alt={'Wroclaw, Poland Photo'}
+          src={'/static/wroclaw.jpg'} />
+      </ReactTooltip>
+    </>)
+  };
 
-    return (
-        <Layout pageTitlePrefix={''}
-                description={'Hey, I’m Andriy Chemerynskiy, a Software Engineer currently based in Wroclaw, Poland.'}
-                imageUrl={'https://andrewchmr.com/static/about.png'}>
-            <div className={'Index'}>
-                <div className={'Index__paragraph'}>Hey, I’m{' '}
-                    <a data-tip={true} data-for="my-photo">Andriy Chemerynskiy</a>
+  return (
+    <Layout pageTitlePrefix={''}
+      description={'Hey, I’m Andriy Chemerynskiy, a Software Engineer currently based in Wroclaw, Poland.'}
+      imageUrl={'https://andrewchmr.com/static/about.png'}>
+      <div className={'Index'}>
+        <div className={'Index__paragraph'}>Hey, I’m{' '}
+          <a data-tip={true} data-for="my-photo">Andriy Chemerynskiy</a>
                     , a Software Engineer
                 </div>
-                <div className={'Index__paragraph'}> currently based in {' '}
-                    <a data-tip={true} data-for="wroclaw-photo">Wroclaw, Poland</a>.
+        <div className={'Index__paragraph'}> currently based in {' '}
+          <a data-tip={true} data-for="wroclaw-photo">Wroclaw, Poland</a>.
                 </div>
-                {renderTooltips()}
-            </div>
-        </Layout>
-    )
+        {renderTooltips()}
+      </div>
+    </Layout>
+  )
 }
 
 export default Index;
