fddf55a9ad1fd8c168a937f80e9474dec5d75546
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2021-04-02 18:06:44 +0200

    Add devto icon

--- src/components/IconsSocial.tsx
+++ src/components/IconsSocial.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 
 export const IconsSocial = () => {
-    return <div className="IconsSocial">
-        <a href='https://www.linkedin.com/in/andriy-chemerynskiy-9a4430139/'
-           className='IconsSocial__linkedin'
-           target="_blank"
-           rel="noopener noreferrer">
-            <img alt='LinkedIn logo' src={'/static/linkedin-logo.svg'}/>
-        </a>
-        <a href='https://github.com/andrewchmr'
-           className='IconsSocial__github'
-           target="_blank"
-           rel="noopener noreferrer">
-            <img alt='Github logo' src={'/static/github-logo.svg'}/>
-        </a>
-        <a href='https://twitter.com/andrewchmr'
-           target="_blank"
-           rel="noopener noreferrer">
-            <img alt='Twitter logo' src={'/static/twitter-logo.svg'}/>
-        </a>
-    </div>
+  return <div className="IconsSocial">
+    <a href='https://www.linkedin.com/in/andriy-chemerynskiy-9a4430139/'
+      className='IconsSocial__linkedin'
+      target="_blank"
+      rel="noopener noreferrer">
+      <img alt='LinkedIn logo' src={'/static/linkedin-logo.svg'} />
+    </a>
+    <a href='https://github.com/andrewchmr'
+      className='IconsSocial__github'
+      target="_blank"
+      rel="noopener noreferrer">
+      <img alt='Github logo' src={'/static/github-logo.svg'} />
+    </a>
+    <a href='https://twitter.com/andrewchmr'
+      className='IconsSocial__twitter'
+      target="_blank"
+      rel="noopener noreferrer">
+      <img alt='Twitter logo' src={'/static/twitter-logo.svg'} />
+    </a>
+    <a href='https://dev.to/andrewchmr'
+      target="_blank"
+      rel="noopener noreferrer">
+      <img alt='Devto logo' src={'/static/devto-logo.svg'} />
+    </a>
+  </div>
 };
